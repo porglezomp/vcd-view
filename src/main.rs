f66515b25e0e99d68d50825877bad305a2d85ef4
C Jones <code@calebjones.net>
2018-08-19 17:16:08 -0400

    Build the waves

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(nll)]
+
 extern crate vcd;
 
 use std::collections::BTreeMap;
@@ -21,19 +23,54 @@ fn main() -> std::io::Result<()> {
     let mut waves: BTreeMap<_, _> = make_waves(&header.items);
     let mut time = 0;
     for command in parser {
-        use Command::*;
-        use Value::*;
         match command {
-            Ok(Timestamp(t)) => time = t,
-            Ok(ChangeVector(id, v)) => waves.get_mut(&id).unwrap().values.push((time, Vector(v))),
-            Ok(ChangeScalar(id, v)) => waves.get_mut(&id).unwrap().values.push((time, Scalar(v))),
+            Ok(Command::Timestamp(t)) => time = t,
+            Ok(Command::ChangeVector(id, v)) => waves
+                .get_mut(&id)
+                .unwrap()
+                .values
+                .push((time, Value::Vector(v))),
+            Ok(Command::ChangeScalar(id, v)) => waves
+                .get_mut(&id)
+                .unwrap()
+                .values
+                .push((time, Value::Scalar(v))),
             _ => (),
         }
     }
-    for wave in waves {
-        println!("{:?}", wave);
+    for wave in waves.values_mut() {
+        for (time, item) in &mut wave.values {
+            normalize(item);
+            if size(item) != wave.var.size as usize {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    format!(
+                        "Value at time {} in {} has invalid width (expected {}, got {})",
+                        time,
+                        wave.var.reference,
+                        wave.var.size,
+                        size(item),
+                    ),
+                ));
+            }
+        }
     }
     Ok(())
+}
+
+fn size(v: &Value) -> usize {
+    match v {
+        Value::Scalar(_) => 1,
+        Value::Vector(v) => v.len(),
+    }
+}
+
+fn normalize(v: &mut Value) {
+    if let Value::Vector(x) = v {
+        if x.len() == 1 {
+            *v = Value::Scalar(x[0]);
+        }
+    }
 }
 
 fn make_waves(items: &[ScopeItem]) -> BTreeMap<IdCode, Wave> {
