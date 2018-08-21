688754a91708db7101ff97b0d82c3ed73d1df6de
C Jones <code@calebjones.net>
2018-08-21 17:33:09 -0400

    Add CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![feature(nll)]
 
+extern crate clap;
 extern crate vcd;
 extern crate web_view;
 
+use clap::{App, Arg};
 use std::collections::BTreeMap;
+use std::io::{self, Write};
 use vcd::{Command, IdCode, Parser, ScopeItem, Var};
 
 mod svg;
@@ -24,8 +27,33 @@ struct Wave {
     svg: Option<svg::Svg>,
 }
 
-fn main() -> std::io::Result<()> {
-    let mut parser = Parser::new(std::io::stdin());
+fn main() -> io::Result<()> {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(clap::AppSettings::ArgRequiredElseHelp)
+        .arg(
+            Arg::with_name("input")
+                .allow_hyphen_values(true)
+                .required(true)
+                .help("The vcd file to read. Use - for stdin."),
+        ).arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .allow_hyphen_values(true)
+                .takes_value(true)
+                .help(
+                    "The output file. If not specified, opens a viewer window. Use - for stdout.",
+                ),
+        ).get_matches();
+
+    let mut parser = match matches.value_of("input").unwrap() {
+        "-" => Parser::new(Box::new(io::stdin()) as Box<dyn io::Read>),
+        fname => Parser::new(Box::new(std::fs::File::open(fname)?) as Box<dyn io::Read>),
+    };
+
     let header = parser.parse_header()?;
     let mut waves: BTreeMap<_, _> = make_waves(&header.items);
     let mut time = 0;
@@ -45,12 +73,13 @@ fn main() -> std::io::Result<()> {
             _ => (),
         }
     }
+
     for wave in waves.values_mut() {
         for &mut (time, ref mut item) in &mut wave.values {
             normalize(item);
             if size(item) != wave.var.size as usize {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidData,
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
                     format!(
                         "Value at time {} in {} has invalid width (expected {}, got {})",
                         time,
@@ -79,24 +108,32 @@ fn main() -> std::io::Result<()> {
         .replacen("$$$DISPLAY$$$", &display_text, 1)
         .replacen("$$$CONTROLS$$$", &vars_text, 1);
 
-    println!("{}", html);
-
-    let size = (1024, 768);
-    let resizable = true;
-    let debug = true;
-    let init_cb = |_webview| {};
-    let frontend_cb = |_webview: &mut _, _arg: &_, _userdata: &mut _| {};
-    let userdata = ();
-    web_view::run(
-        "Waveform",
-        web_view::Content::Html(html),
-        Some(size),
-        resizable,
-        debug,
-        init_cb,
-        frontend_cb,
-        userdata,
-    );
+    match matches.value_of("output") {
+        Some("-") => {
+            print!("{}", html);
+        }
+        Some(output) => {
+            std::fs::File::create(output)?.write(html.as_bytes())?;
+        }
+        None => {
+            let size = (1024, 768);
+            let resizable = true;
+            let debug = true;
+            let init_cb = |_webview| {};
+            let frontend_cb = |_webview: &mut _, _arg: &_, _userdata: &mut _| {};
+            let userdata = ();
+            web_view::run(
+                "Waveform",
+                web_view::Content::Html(html),
+                Some(size),
+                resizable,
+                debug,
+                init_cb,
+                frontend_cb,
+                userdata,
+            );
+        }
+    }
 
     Ok(())
 }
